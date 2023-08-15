f9d00e99466d47cc9b65bc5ff48b071ee1326140
codecustard <109494546+vimalveeramani@users.noreply.github.com>
2023-08-15 10:11:23 +0530

    Create arithmetic.r
    arithmetic operators

---
+++ arithmetic.r
@@ -0,0 +1,14 @@
+#Arithmetic operators
+a + b #Sums two variables
+
+a -  b #Subtracts two variables
+
+a * b #Multiply two variables
+
+a / b #Divide two variables
+
+a ^ b #Exponentiation of a variable
+
+a%%b #Remainder of a variable
+
+a%/%b #Integer division of variables
