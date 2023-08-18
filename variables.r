4e64648d5737be0f891bb52f92c52a794baea05d
codecustard <109494546+vimalveeramani@users.noreply.github.com>
2023-08-18 09:03:49 +0530

    Update variables.r

--- variables.r
+++ variables.r
@@ -5,3 +5,19 @@ age <- 21
 print(age)
 v1<-v2<-v3<- "hai"
 #we can assign multiple values to the same variables like above
+# Legal variable names:
+myvar <- "John"
+my_var <- "John"
+myVar <- "John"
+MYVAR <- "John"
+myvar2 <- "John"
+.myvar <- "John"
+
+# Illegal variable names:
+2myvar <- "John"
+my-var <- "John"
+my var <- "John"
+_my_var <- "John"
+my_v@ar <- "John"
+TRUE <- "John"
+
