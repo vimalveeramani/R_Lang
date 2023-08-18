942f2654f0b0c6c50ec90eff9492fde05a08ab13
codecustard <109494546+vimalveeramani@users.noreply.github.com>
2023-08-18 09:00:51 +0530

    Create paste.r

---
+++ paste.r
@@ -0,0 +1,12 @@
+# paste function usage 
+text <- "nobody"
+paste("my name is",text)
+# You can concatenate, or join, two or more elements, by using the paste() function.
+# To combine both text and a variable, R uses comma (,)
+t1 <- "hey"
+t2 <- "you!"
+paste(t1,t2)
+# You can also use , to add a variable to another variable:
+
+
+
